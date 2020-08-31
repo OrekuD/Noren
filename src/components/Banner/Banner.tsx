f6572102b707d0cc50a8e3d32bea251af57e7cee
unknown <opokudavid446@gmail.com>
2020-08-31 18:47:05 +0100

    Add banner component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,13 +2,24 @@ import React from "react";
 import "./Banner.scss";
 import Header from "../Header/Header";
 import Pattern from "../../assets/images/pattern.png";
+import Hero from "../../assets/images/hero.png";
 
 const Banner = () => {
   return (
     <div className="banner">
       <Header />
       <img src={Pattern} alt="" className="pattern" />
-      <div className="content"></div>
+      <div className="content">
+        <div className="hero-content">
+          <p className="title">Get business solutions with Noren.</p>
+          <p className="sub-title">
+            Power your business to new heights with our award-winning digital
+            marketing services and technology platform.
+          </p>
+          <button>Get Started</button>
+        </div>
+        <img src={Hero} alt="" className="hero" />
+      </div>
     </div>
   );
 };
